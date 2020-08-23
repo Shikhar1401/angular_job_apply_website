a69e68090de7bf52abe4597269c6f71e75304bcd
shikhar1401 <shikharagarwal0717@gmail.com>
2020-08-23 22:35:36 +0530

    security

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyDLby-sMuNr4qh9Xi2Voa5BuXtaQgQUCm0",
-    authDomain: "naukri-simplified.firebaseapp.com",
-    databaseURL: "https://naukri-simplified.firebaseio.com",
-    projectId: "naukri-simplified",
-    storageBucket: "naukri-simplified.appspot.com",
-    messagingSenderId: "639506976510",
-    appId: "1:639506976510:web:a658490d8a889da1069faf",
-    measurementId: "G-DKXDDCN15T"
-  }
+  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyDLby-sMuNr4qh9Xi2Voa5BuXtaQgQUCm0",
-    authDomain: "naukri-simplified.firebaseapp.com",
-    databaseURL: "https://naukri-simplified.firebaseio.com",
-    projectId: "naukri-simplified",
-    storageBucket: "naukri-simplified.appspot.com",
-    messagingSenderId: "639506976510",
-    appId: "1:639506976510:web:a658490d8a889da1069faf",
-    measurementId: "G-DKXDDCN15T"
-  }
 };
 
 /*
